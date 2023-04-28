6794aa6920d9b2c89eae0df9bcd4ca655e9d696f
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-04-28 12:34:43 +0500

    starter

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const header = document.querySelector(".header");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  'We use cookie for improved functionality and analytics <button class = "btn btn--close-cookie">Got it </button>';
+header.append(message);
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+message.style.backgroundColor = "#37383d";
+message.style.width = "104%";
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
